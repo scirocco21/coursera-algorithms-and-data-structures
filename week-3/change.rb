dec92be9743fac372d59a330ecd8ae1cef4c6556
Sebastian <sebgertz@gmail.com>
2018-10-05 15:21:34 -0400

    solved first problem

--- week-3/change.rb
+++ week-3/change.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/env ruby
 # by Andronik Ordian
-
 def get_change(n)
   count = 0
   coins = [1, 5, 10]
-  # write your code here
-  count
+  while n > 0
+    if n >= 10
+      n -= 10
+    elsif n >= 5
+      n -= 5
+    else
+      n -= 1
+    end
+    count += 1
+  end
+  return count
 end
 
 if __FILE__ == $0
   n = gets.to_i
-  puts "#{get_change(n)}"   
+  puts "#{get_change(n)}"
 end
