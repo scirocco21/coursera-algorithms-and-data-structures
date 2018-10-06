fb530619e9224635a6838803603789ba67d591a8
Sebastian <sebgertz@gmail.com>
2018-10-06 00:25:20 -0400

    solved different summands problem

--- week-3/different_summands.rb
+++ week-3/different_summands.rb
@@ -1,10 +1,20 @@
-#!/usr/bin/env ruby
-# by Andronik Ordian
-
 def optimal_summands(n)
   summands = []
-  
-  summands
+  temp = 0
+  i = 1
+  while i <= n
+    summands.push(i)
+    temp += i
+    i += 1
+
+    if temp > n
+      excess = temp - n
+      summands.delete(excess)
+      return summands
+    elsif temp == n
+      return summands
+    end
+  end
 end
 
 if __FILE__ == $0
